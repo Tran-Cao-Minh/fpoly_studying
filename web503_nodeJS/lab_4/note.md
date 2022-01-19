6361932bc3e2560db978818009d5bb41bda7172d
Trần Cao Minh <trancaominh932016@gmail.com>
2022-01-19 17:02:52 +0700

    Update lab 4 Node JS

--- web503_nodeJS/lab_4/note.md
+++ web503_nodeJS/lab_4/note.md
@@ -9,15 +9,34 @@ npm i http-errors
 Done $ npm start
 
 # 4. Tạo controller trong nodejs
-Done 
 a. routes/category.js
 b. app.js
 
 # 5. Tạo model
 npm i mysql
-database.js
+models/database.js
 
 # 6. Nhúng model vào controller
 routes/category.js - require database
 
 # 7. Chức năng hiện danh sách record
+a. routes/category.js - /
+b. views/category-list.ejs
+
+# 8. Chức năng hiện form thêm mới
+a. routes/category.js - /add
+b. views/category-add.ejs
+
+# 9. Chức năng lưu form vào database
+a. routes/category.js - /store
+
+# 10. Chức năng hiện form edit 1 record
+a. routes/category.js - /edit/:id
+b. views/category-edit.ejs
+
+# 11. Code cập nhật record vào database
+routes/category.js - /update
+
+# 12. Chức năng xóa 1 record trong database
+routes/category.js - /delete/:id
+
--- web503_nodeJS/lab_4/routes/category.js
+++ web503_nodeJS/lab_4/routes/category.js
@@ -2,32 +2,106 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/database');
 
-router.get('/', function(req, res, next) {
-  let sql = ### 
-  Chức năng hiện danh sách record
-  res.send('Book category list');
+router.get('/', function (req, res, next) {
+  db.query(
+    `
+      SELECT PkCategory_Id, CategoryName, CategoryOrder, CategoryDisplay
+      FROM product_category
+    `,
+    function (err, data) {
+      res.render('category-list', {
+        categoryList: data
+      });
+    }
+  );
 })
 
-router.get('/add', function(req, res, next) {
-  res.send('Adding book category Form');
+router.get('/add', function (req, res, next) {
+  res.render('category-add');
 })
 
-router.post('/store', function(req, res, next) {
+router.post('/store', function (req, res, next) {
   // get data form add router to add record into database
+  let categoryName = req.body.categoryName;
+  let categoryOrder = req.body.categoryOrder;
+  let categoryDisplay = req.body.categoryDisplay;
+
+  let productCategory = {
+    CategoryName: categoryName,
+    CategoryOrder: categoryOrder,
+    CategoryDisplay: categoryDisplay,
+  };
+
+  db.query(
+    `INSERT INTO product_category SET ?`, productCategory,
+    function (err, data) {
+      if (err) throw err;
+      res.redirect('/category');
+    }
+  )
 })
 
-router.get('/edit/:id', function(req, res, next) {
+router.get('/edit/:id', function (req, res, next) {
   let id = req.params.id;
-  res.send('Edit book category Form ' + id);
+
+  db.query(
+    `
+      SELECT PkCategory_Id, CategoryName, CategoryOrder, CategoryDisplay
+      FROM product_category 
+      WHERE PkCategory_Id = ${id}
+      LIMIT 1
+    `,
+    function (err, data) {
+      res.render('category-edit', {
+        category: data[0]
+      });
+    }
+  )
 })
 
-router.post('/update', function(req, res, next) {
+router.post('/update', function (req, res, next) {
   // get data from edit router to update into database
+  let PkCategory_Id = req.body.categoryId;
+  let categoryName = req.body.categoryName;
+  let categoryOrder = req.body.categoryOrder;
+  let categoryDisplay = req.body.categoryDisplay;
+
+  let productCategory = {
+    CategoryName: categoryName,
+    CategoryOrder: categoryOrder,
+    CategoryDisplay: categoryDisplay,
+  };
+
+  db.query(
+    `
+      UPDATE product_category SET ?
+      WHERE PkCategory_Id = ${PkCategory_Id}
+    `,
+    productCategory,
+    function (err, data) {
+      if (data.affectedRows === 0) {
+        console.log(`Do not have category with ID ${PkCategory_Id} to update`);
+      };
+      res.redirect('/category');
+    }
+  )
 })
 
-router.get('/delete/:id', function(req, res) {
+router.get('/delete/:id', function (req, res) {
   let id = req.params.id;
-  res.send('Delete book category');
+
+  db.query(
+    `
+      DELETE FROM product_category
+      WHERE PkCategory_Id = ${id}
+    `,
+    function (err, data) {
+      if (data.affectedRows === 0) {
+        console.log(`Do not have category with ID ${PkCategory_Id} to delete`);
+      };
+      res.redirect('/category');
+    }
+  )
 })
 
 module.exports = router;
