018b6cb47c76aa603334bc9ffc441b6e05f4e81a
Trần Cao Minh <trancaominh932016@gmail.com>
2022-02-07 16:56:36 +0700

    Update lab 5 node js

--- web503_nodeJS/lab_5/note.md
+++ web503_nodeJS/lab_5/note.md
@@ -3,7 +3,7 @@
 chuyến các kiến thức đã học được sang tiếng Anh
 
 # 1. Chuẩn bị Database
-3 table gồm: product, product_category, user
+2 table gồm: product, product_category
 - có nâng cấp thêm các trường mới cho dữ liệu phong phú
 - tên các cột đặt theo quy chuẩn bằng tiếng Anh
 - các bảng có quan hệ và ràng buộc rõ ràng
@@ -31,11 +31,11 @@ Vào thư mục web503_nodeJS, mở terminal
 # 5. Tạo route (product thay sách)
 tại file app.js dòng 25, 31
 tạo file routes/product.js
-- format đẹp
+- format đẹp, tiếng Anh
 
-# 6. Thực hiện chức năng danh sách record
+# 6. Thực hiện chức năng danh sách record - product
 tại router get dòng 7 - file routes/product.js
-- gộp sql cùng hàm query, code đẹp
+- gộp sql cùng hàm query, code đẹp, tiếng Anh
 - gọi tất cả dữ liệu ra res.json
 - tìm hiểu res.json có thể format, định dạng khác với res.send
 test với postman, gọi ra. 
@@ -43,7 +43,44 @@ test với postman, gọi ra.
 http://localhost:3000/product - method: GET
 (collection - lab 5 node js)
 
-# 7. Thực hiện chức năng thêm record
+# 7. Thực hiện chức năng thêm record - product
+tại router post dòng 26 - file routes/product.js
+- code đẹp, tiếng Anh
+- có thể upload file ảnh, chỉnh sửa tên ảnh không bị trùng
+- làm sạch dữ liệu với các hàm
+- xóa file ảnh lưu trên bộ nhớ đệm
+test với postman, gọi ra.
+- collection: lab 5 node js
+- tên gợi nhớ: add product
+http://localhost:3000/product - method: POST
+truyền tham số tại mục Body > form-data
+
+# 8. Thực hiện chức năng cập nhật record - product
+tại router put/:id dòng 84 - file routes/product.js
+- code đẹp, tiếng Anh
+- có thể upload file ảnh, chỉnh sửa tên ảnh không bị trùng
+- làm sạch dữ liệu với các hàm
+- xóa file ảnh lưu trên bộ nhớ đệm
+- xóa file ảnh cũ, tối ưu tốc độ với LIMIT 1
+test với postman, gọi ra
+*** cần cập nhật đúng ID
+- collection: lab 5 node js
+- tên gợi nhớ: update product
+http://localhost:3000/product/id - method: PUT
+truyền tham số tại mục Body > form-data
+
+# 9. Thực hiện chức năng xóa record
+tại router delete/:id dòng 174 - file routes/product.js
+- code đẹp, tiếng Anh
+- xóa file ảnh theo sản phẩm bị xóa, tối ưu tốc độ với LIMIT 1
+test với postman, gọi ra
+*** cần cập nhật đúng ID
+- collection: lab 5 node js
+- tên gợi nhớ: delete product
+http://localhost:3000/product/id - method: DELETE
+
+
+
 
 
 
--- web503_nodeJS/lab_5/routes/product.js
+++ web503_nodeJS/lab_5/routes/product.js
@@ -38,7 +38,7 @@ router.post('/', function (req, res, next) {
     let productDimensions = fields.productDimensions.trim();
     let productPages = fields.productPages;
     let productDisplay = fields.productDisplay;
- 
+
     let date = new Date();
     let slug = productName.toLowerCase().replace(/[^A-Za-z0-9\ ]/g, '').replace(/[\ ]+/g, '-');
     let prefix = slug + '_' + date.getTime();
@@ -49,10 +49,10 @@ router.post('/', function (req, res, next) {
     fs.copyFile(pathFile, destPath, (err) => {
       if (err) {
         throw err;
-      }
+      };
       fs.unlink(pathFile, () => {
         console.log('Temp file is deleted ' + pathFile);
-      })
+      });
       console.log('Uploaded file ' + fileName);
     });
 
@@ -75,10 +75,136 @@ router.post('/', function (req, res, next) {
         throw err;
       };
       res.json({
-        'notification' : 'add product completed',
+        'notification': 'add product completed',
       });
     });
   });
+});
+
+// update product
+router.put('/:id', function (req, res, next) {
+  let PkProduct_Id = req.params.id;
+
+  db.query(
+    `
+      SELECT ProductImage
+      FROM product 
+      WHERE PkProduct_Id = ${PkProduct_Id}
+      LIMIT 1
+    `,
+    function (err, data) {
+      let form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        let productCategory = fields.productCategory;
+        let productName = fields.productName.trim();
+        let productPrice = fields.productPrice;
+        let productDescription = fields.productDescription.trim();
+        let productPublisher = fields.productPublisher.trim();
+        let productPublishDate = fields.productPublishDate;
+        let productDimensions = fields.productDimensions.trim();
+        let productPages = fields.productPages;
+        let productDisplay = fields.productDisplay;
+
+        let checkUpdateProductImage = files.productImage.size;
+        let fileName = data[0].ProductImage;
+        if (checkUpdateProductImage !== 0) {
+          let pathFile = files.productImage.filepath;
+          let fileExtension = files.productImage.mimetype.split('/').pop();
+
+          let date = new Date();
+          let slug = productName.toLowerCase().replace(/[^A-Za-z0-9\ ]/g, '').replace(/[\ ]+/g, '-');
+          let prefix = slug + '_' + date.getTime();
+          fileName = prefix + '.' + fileExtension;
+          let destPath = __dirname.replace('\\routes', '') + '\\public\\images\\' + fileName;
+          console.log(destPath);
+
+          fs.copyFile(pathFile, destPath, (err) => {
+            if (err) {
+              throw err;
+            };
+            fs.unlink(pathFile, () => {
+              console.log('Temp file is deleted ' + pathFile);
+            });
+            console.log('Uploaded file ' + fileName);
+
+            let oldImageFileName = data[0].ProductImage;
+            let oldImageFileNamePath =
+              __dirname.replace('\\routes', '') + '\\public\\images\\' + oldImageFileName;
+            fs.unlink(oldImageFileNamePath, () => {
+              console.log('Old image file is deleted ' + oldImageFileNamePath);
+            });
+          });
+        };
+
+        const product = {
+          FkCategory_Id: productCategory,
+          ProductName: productName,
+          ProductPrice: productPrice,
+          ProductDescription: productDescription,
+          ProductPublisher: productPublisher,
+          ProductPublishDate: productPublishDate,
+          ProductDimensions: productDimensions,
+          ProductPages: productPages,
+          ProductDisplay: productDisplay,
+          ProductImage: fileName,
+        };
+        console.log(product);
+
+        db.query(
+          `
+            UPDATE product SET ?
+            WHERE PkProduct_Id = ${PkProduct_Id}
+            LIMIT 1
+          `,
+          product,
+          function (err, data) {
+            if (err) {
+              throw err;
+            };
+            res.json({
+              'notification': 'update product completed',
+            });
+          }
+        );
+      });
+    }
+  );
+});
+
+// delete product
+router.delete('/:id', function (req, res) {
+  let PkProduct_Id = req.params.id;
+
+  db.query(
+    `
+      SELECT ProductImage
+      FROM product 
+      WHERE PkProduct_Id = ${PkProduct_Id}
+      LIMIT 1
+    `,
+    function (err, data) {
+      let oldImageFileNamePath =
+        __dirname.replace('\\routes', '') + '\\public\\images\\' + data[0].ProductImage;
+      fs.unlink(oldImageFileNamePath, () => {
+        console.log('Old image file is deleted ' + oldImageFileNamePath);
+      });
+
+      db.query(
+        `
+          DELETE FROM product
+          WHERE PkProduct_Id = ${PkProduct_Id}
+        `,
+        function (err, data) {
+          if (err) {
+            throw err;
+          };
+          res.json({
+            'notification': 'delete product completed',
+          });
+        }
+      );
+    }
+  );
 });
 
 module.exports = router;
