9a575815b9fd60bf0d5feb1f30981f75bc3f2c72
Tran Cao Minh <minhtc.dev.workspace@gmail.com>
2022-07-02 08:28:37 +0700

    update lab 1 react

--- web209_react/lab-1/ex-3/src/MainContent.js
+++ web209_react/lab-1/ex-3/src/MainContent.js
@@ -1,5 +1,23 @@
 function MainContent() {
-  return <h1>Main Content</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <h1 style={{ width: "100%", textAlign: "center", marginBottom: "22px" }}>
+        Main Content
+      </h1>
+      <img
+        style={{ textAlign: "center", height: "300px" }}
+        src="./images/happy-programmers-day.jpg"
+        alt="Happy Programmers Day"
+      />
+    </div>
+  );
 }
 
 export default MainContent;
